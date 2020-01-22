06d138a2c87b363c72e033ecfbd7b293a79f3a39
manoj2270 <rgu.manoj@gmail.com>
2020-01-22 16:42:23 +0530

    adding loaddata class

--- load_data.py
+++ load_data.py
@@ -1,1 +1,14 @@
 #class to load the data from different input sources
+
+import numpy as np
+import pandas as pd
+#import matplotlib.pyplot as plt
+
+class load_data():
+
+    def __init__(self,file):
+        self.filename = file
+
+
+if __name__ == "__main__":
+    data = load_data("test.csv")
